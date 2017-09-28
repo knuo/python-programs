396daa85037c0f5f8198d67c5f8cc40587b1e595
knuo <31614730+knuo@users.noreply.github.com>
2017-09-28 09:12:25 +0530

    Update karan4.py

--- karan4.py
+++ karan4.py
@@ -1,5 +1,7 @@
-#multiplication
+#multiplication tables using python
 #!/usr/bin/python3
+#integers from range 1 to hundred
 for i in range(1,100):
+	#printing the tables
 	print(i*1,'\t',i*2,'\t',i*3,'\t',i*4,'\t',i*5,'\t',i*6,'\t',i*7,'\t',i*8,'\t',i*9,'\t',i*10,'\t')
 	
